72dcfc4f4da525fa994cbad5091ca49b7a2a5bac
neketli <neketli.cspl@gmail.com>
2022-09-26 20:38:34 +0300

    ssr: false to capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.pomodoroappnuxt.capapp',
   appName: 'pomodoro-app-nuxt',
-  webDir: 'www',
+  webDir: 'dist',
   bundledWebRuntime: false
 };
 
